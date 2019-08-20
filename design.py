0f218c304db80e2a7e6aabe1801ad548ecd3ce7a
Shane Whiteman <shanewhiteman@shanes-mbp.lan>
2019-08-19 20:25:40 -0400

    Added a process of saving/storing the contents of the create function into a dictionary within the function

--- design.py
+++ design.py
@@ -1,40 +1,94 @@
 import sys
-import json
-#prompt a user to create a recipe
-#Ask for Ingredients
-#Ask for Instructions
+
 #Save/Store it into dictionary
 def create():
-    ask_recipe = ''
-    ask_instruction = ''
-    ask_ingredients = ''
-    recipe_dict = dict()
-    #counter = str(1)
-    print("Create a recipe! Type 'exit' to leave")
-    for ask_recipe in range (1):
-        ask_recipe = input("Recipe Name: ")
-        recipe_dict.update(ask_recipe = 'Nothing Yet')
-        if ask_recipe == 'exit':
-            exit(1)
+    
+    recipe_name = ''
+    recipe_dict = {}
 
-    while ask_instruction != 'exit':
-        ask_instruction = input("Instructions: ")
-        if ask_instruction == 'exit':
-            while ask_ingredients != 'exit':
-                ask_ingredients = input("Ingredients: ")  
-                if ask_ingredients == 'exit':
-                    #recipe_dict.update(ask_recipe = ask_instruction)
-                    #recipe_dict.update(ask_recipe, ask_ingredients)
-                    for key, value in recipe_dict.items():
-                        print(key,':',value)
-                        exit(1)
+    print("Create a recipe!")
+    recipe_name = input("Recipe Name: ")
+
+    ingredientList = []
+    ingredients = ''
+
+    while True:
+        ingredients = input("Ingredients: ")  
+        if ingredients == 'exit':
+            break
+        ingredientList.append(ingredients)
 
 
+    instructionList = []
+    instructions = ""
 
-    print("Create a recipe! Enter '0' to exit")
-    print(ask_recipe)
-    print(ask_instruction)
-    print(ask_ingredients)
+    while True:
+        instructions = input("Instructions: ")
+        if instructions == 'exit':
+            break
+        instructionList.append(instructions)
+
+    class ingr_instr_list:
+        ingrSave = ingredientList
+        instrSave = instructionList
+        #ingrTest = print(*ingredientList)
+        #instrTest = print(*instructionList)
+
+    save = ingr_instr_list
+    
+    recipe_dict[recipe_name] = save
+
+    
+
+
+#Step1: define the class
+#Step2: Init the class
+#Step3: Insert data into new class object
+#Step4: key(recipe_name) -> class
+#Step5: use dictionary to retrieve recipes objects and manipulate their contents
+
+
+#animalDictionary = {}
+
+#class Animal:
+    #speak = ""
+
+#name = input("What is your animal?: ")
+#sound = input("What does it say?: ")
+#user_input == "meow"
+
+#some_animal = new Animal();
+#some_animal.speak = sound
+#key(animal name) -> value("a new animal")
+#animalDictionary[name] = some_animal
+
+#-----
+
+#animal = animalDictionary["wowzers"]
+#animal.speak #meow
+#animal.speak = "mooo"
+#animalDictionary["wowzers"] = animal
+
+#    "some_string_here" -> "wow"
+
+#    "apple_pie" -> apple_pie
+
+#    class recipe:
+#        name: string
+#        instructions: []string
+#        ingredients: []string
+
+
+#     apple_pie = new recipe();
+#     apple_pie.name = "apple pie merg"
+#     apple_pie.instruction = []string
+#     apple_pie.ingredients = []string
+
+#     someRecipe = recipeDict["apple_pie"]
+#     apple_pie
+
+#     apple_pie.open = False 
+#     print(apple_pie.open)
 
 def listrecipe():
     print("listrecipe")
